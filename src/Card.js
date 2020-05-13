e3d77cb37a39fb3919c016afa3c4eb9b501668cb
tomashei <tomasz_czarnecki@outlook.com>
2020-05-13 19:08:40 +0200

    update comments

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {  //can also work as: const Card = ({ name, email, id }
     const { name, email, id } = props; //allow not to use props.name, props.email, props.id
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='card' src={`https://robohash.org/${name}?200x200`} />
+            <img alt='card' src={`https://robohash.org/${id}?200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
